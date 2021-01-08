85297c0b6cf9a68a6c10f79933500fb1741ab7a0
Zekarias Negassa <zakfena@gmail.com>
2021-01-08 16:01:57 -0600

    resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,27 +1,25 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Wrapper from "../components/Wrapper";
-import "./Resume.css"
+import "./Resume.css";
 
 function Resume() {
   return (
-    
+    <div>
       <div class="card w-60">
+        <a className= "download" href="./Resume-Zekarias-Negassa.pdf" width="100%" height="300%" download>
+          Download Resume{" "}
+        </a>
         <object
           className="resumePage"
           data="./Resume-Zekarias-Negassa.pdf"
           type="application/pdf"
           width="80%"
-          height="500px"
+          height="600px"
         ></object>
-        {/* <a href ="Resume.Zekarias.Negassa.pdf" width="100%" height="300%">
-
-          </a> */}
-          <Footer />
       </div>
-
-      
-    
+      <Footer />
+    </div>
   );
 }
 
